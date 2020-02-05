2bd18d703843a73d0b3791dd5b22b2d20ac70540
Jenaba Sow <jenabasow@gmail.com>
2020-02-04 21:09:53 -0500

    start reverse if statement

--- csci350.scm
+++ csci350.scm
@@ -1,1 +1,5 @@
 ;1. (25 pts) Write a function (reverse-general L). L is a list. The result of the function is the reversed version of L. Every single sub-list in L should be reversed as well.
+(define (reverse-general L)
+        (if (null? L)
+            '()
+            (if (list? (car L)) ;car = contents of address register holds the first value of the list
